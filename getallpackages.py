9f7d26d2b1ded73538480a4fb0f0c6fc2c6ce4b7
Dan Elia <d.elia@outlook.com>
2023-10-12 12:33:12 +0000

    prototype of all package creator (gets latest version only)
    Former-commit-id: 7aaf9c60eb0efc78367e4d1a525e3e98a33f55cd

--- getallpackages.py
+++ getallpackages.py
@@ -1,10 +1,10 @@
 import hashlib
+import time
 import requests
 import pyreadr
 import pandas as pd
 import os
 import json
-from packaging.version import parse
 import pickle
 import email.utils, datetime
 
@@ -60,8 +60,28 @@ locations = {}
 for i in builtin:
 	packageVersions[i["name"]] = i["versions"]
 	locations[i["name"]] = i["file"]
-for cranPackage in pandasDatabase["Package"]:
-	record = pandasDatabase.loc[pandasDatabase['Package'] == cranPackage]
+
+progress = 0
+total = 0
+
+def writePackage(package):
+	global progress, total
+	progress += 1
+
+	record = pandasDatabase.loc[pandasDatabase['Package'] == package]
+	urlbool = True
+
+	if len(record) == 0:
+		if packagesBIOC.get(package) == None:
+			raise Exception(f"Package {'r-' + package.lower().replace('.','-')} not found in database")
+		else:
+			record = packagesBIOC[package]
+			packman = "bioc"
+	else:
+		record = record.to_dict('records')[0]
+		packman = "cran"
+
+	name, description = record["Title"], record["Description"].replace("\\", "")
 
 	def writeDeps(field):
 		if record.get(field) == None:
@@ -75,9 +95,118 @@ for cranPackage in pandasDatabase["Package"]:
 	dependencies += writeDeps("LinkingTo")
 	suggests = writeDeps("Suggests")
 
-	if "r-" + cranPackage.lower().replace(".","-") in packageVersions.keys():
-		if not record["Version"] in packageVersions["r-" + cranPackage.lower().replace(".","-")]:
-			#TODO: Add version checking
-			continue
+	try:
+		packageURL = record["URL"].split(",")[0].split(" ")[0].split("\n")[0].replace("\"", "")
+	except:
+		urlbool = False
+	
+	if "r-" + package.lower().replace(".","-") in packageVersions.keys():
+		if record["Version"] in packageVersions["r-" + package.lower().replace(".","-")]:
+			print(f"({'{:.2f}'.format((progress/total) * 100)}%) Package {'r-' + package.lower().replace('.','-')} already exists")
+			return()
+		else:
+			pass
 	else:
-		continue
+		pass
+
+	baseurl_latest = f"https://cran.r-project.org/src/contrib/{package}_{record['Version']}.tar.gz"
+	latest = requests.get(baseurl_latest, allow_redirects=True)
+	sha256_hash_latest = hashlib.sha256()
+	sha256_hash_latest.update(latest.content)
+	versions = (f"""	version("{record['Version']}", sha256="{sha256_hash_latest.hexdigest()}")\n""")
+
+	dependencylist = []
+	variants = []
+	for k in dependencies:
+		try:
+			dependencylist.append("\tdepends_on(\"" + packageName(k)[0] + "\", type=(\"build\", \"run\"))\n")
+		except:
+			continue
+	for l in suggests:
+		try:
+			pkg = packageName(l)
+			if pkg[0] == ('r-' + package.lower().replace('.','-')):
+				continue
+			dependencylist.append("\tdepends_on(\"" + pkg[0] + pkg[1] + "\", when=\"+" + pkg[0] + "\", type=(\"build\", \"run\"))\n")
+			variants.append("\tvariant(\"" + pkg[0] + "\", default=" + str(pkg[3]) + ", description=\"Enable " + pkg[0] + " support\")\n")
+		except:
+			continue
+
+	classname = package.split(".")
+	for i in range(len(classname)):
+		classname[i] = classname[i].capitalize()
+	classname = "".join(classname)
+
+	os.makedirs("packages/r-" + package.lower().replace(".","-"))
+
+	f = open("packages/r-" + package.lower().replace(".","-") + "/package.py", "w")
+	f.write(f"""# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+	
+from spack.package import *
+	
+			
+class R{classname}(RPackage):
+	\"\"\"{name}
+
+	{description}
+	\"\"\"
+	
+	{f'homepage = "{packageURL}"' if urlbool and str(packageURL) != "nan" else ''}
+	{packman} = "{package}"
+
+{"".join(versions)}
+
+{"".join(dependencylist)}
+{"".join(variants)}
+""")
+
+	f.close()
+	print(f"({'{:.2f}'.format((progress/total) * 100)}%) Package {'r-' + package.lower().replace('.','-')} has been created!")
+
+def packageName(k):
+	if "(" in k:
+		version = k.split("(")
+		k = version[0]
+		version = version[1].lower().replace(")","")
+		getversion = ""
+		if ">=" in version:
+			fullname = k.replace(".","-")
+			getversion = f"@{version[2:]}:"
+			type = "at least " + version[2:]
+		elif "<=" in version:
+			fullname = k.replace(".","-")
+			getversion = f"@:{version[2:]}"
+			type = version[2:]
+		else:
+			fullname = k.replace(".","-")
+			getversion = f"@={version[1:]}"
+			type = version[2:]
+	else:
+		fullname = k.replace(".","-")
+		getversion = ""
+		type = "any"
+	if fullname.lower() == "r":
+		return "r", getversion, type, False
+	record = pandasDatabase.loc[pandasDatabase['Package'] == k]
+	if len(record) == 0:
+		if packagesBIOC.get(k) == None:
+			raise Exception(f"Package {'r-' + k.lower().replace('.','-')} not found in database")
+	# print(fullname.lower(), getversion, type)
+	return "r-" + fullname.lower(), getversion, type, False
+
+def packageLoop(lib, libname):
+	print(f"Creating {libname} packages...")
+	global progress, total
+	progress = 0
+	total = len(lib)
+	for i in lib:
+		writePackage(i)
+		#time.sleep(1)
+	print(f"Finished creating {libname} packages!")
+
+
+packageLoop(packagesBIOC.keys(), "Bioconductor")
+packageLoop(pandasDatabase["Package"], "CRAN")
