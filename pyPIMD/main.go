34ccc300c8fe9f5be796b099f3927e4fd8b447b5
Michael Woolnough <mw31@sanger.ac.uk>
2024-03-06 14:02:56 +0000

    Add command to grab METADATA file from Python Wheels.
    Former-commit-id: 0aff0654e6ac8bcdc72892464282891e680e9fcb

---
+++ pyPIMD/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	bufra "github.com/avvmoto/buf-readerat"
+	"github.com/snabb/httpreaderat"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
+func run() error {
+	req, _ := http.NewRequest("GET", os.Args[1], nil)
+
+	htrdr, err := httpreaderat.New(nil, req, nil)
+	if err != nil {
+		panic(err)
+	}
+	bhtrdr := bufra.NewBufReaderAt(htrdr, 1024*1024)
+
+	z, err := zip.NewReader(bhtrdr, htrdr.Size())
+
+	for _, f := range z.File {
+		if strings.HasSuffix(f.Name, "info/METADATA") {
+			r, err := f.Open()
+			if err != nil {
+				return err
+			}
+
+			_, err = io.Copy(os.Stdout, r)
+			if err != nil {
+				return err
+			}
+
+			break
+		}
+	}
+
+	return nil
+}
